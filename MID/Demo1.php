2da19aefa04d4810a158e2e633b1b5abe1aa8785
Ash Raf <rifo0118@gmail.com>
2025-07-24 19:34:25 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ MID/Demo1.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AIUB|Log in</title>
+</head>
+
+<body>
+    <div class="center">
+        <p class="t"><b>American international university of Banglaesh</b></p>
+        <table>
+            <td>
+                <tr>
+                    <p class="t1"> -Where leaers are created.</p>
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    <p class="t2">Sign in with your organizational id number.</p>
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    <input class="Ubox" type="text" placeholder="Student ID">
+                </tr>
+            </td>
+            <td>
+                <tr>
+                    <input class="Pbox" type="text" placeholder="Password">
+                </tr>
+            </td>
+
+            <td>
+                <tr>
+                    <button class="Lbutton">Log In</button>
+                </tr>
+            </td>
+
+            <td>
+                <tr>
+                    <a href="https://portal.aiub.edu/" class="link">Can’t access your account?</a>
+                </tr>
+            </td>
+        </table>
+    </div>
+
+
+
+
+
+    <style>
+    body {
+        Font-Family: arial;
+        background-color: #dddddd;
+        text-align: center;
+
+    }
+
+    div {
+        border: 1px solid #dddddd;
+        padding: 30px;
+        height: 300px;
+        width: 18%;
+        background-color: #ffffff;
+
+
+
+
+    }
+
+    .center {
+        margin-top: 150px;
+        margin-bottom: 100px;
+        margin-right: 2500px;
+        margin-left: 550px;
+        width: 18%;
+        border: 1px solid #dddddd;
+        padding: 50px;
+    }
+
+    .t {
+        Font-Size: 15px;
+        margin: top 10px;
+        ;
+        color: #031649ff;
+        font-family: Tahoma, Verdana, sans-serif;
+    }
+
+    .t1 {
+        Font-Size: 10px;
+        margin-top: 5px;
+        color: #7e7d7dff;
+    }
+
+    .t2 {
+        Font-Size: 12px;
+        display: inline-block;
+        margin-top: 25px;
+        color: #000000ff;
+    }
+
+
+
+    .Ubox {
+        margin-top: 30px;
+        height: 25px;
+        width: 90%;
+        border-style: round;
+        border: 1px solid #6082ccff;
+        border-radius:5px;
+        display: inline-block;
+
+    }
+
+    input::placeholder {
+        color: #7e7d7dff;
+        opacity: 0.5;
+        font-family: Tahoma, Verdana, sans-serif;
+        font-size: 10px;
+    }
+
+    .Pbox {
+        display: inline-block;
+        margin-top: 25px;
+        height: 25px;
+        width: 90%;
+        border-style: round;
+        border: 1px solid #6082ccff;
+        border-radius:5px;
+        
+
+    }
+
+    .Lbutton {
+        margin-top: 8px;
+        height: 35px;
+        width: 92.5%;
+        border-style: round;
+        border: 0px solid #160101ff;
+        color: #ffffffff;
+        font-family: Tahoma, Verdana, sans-serif;
+        font-size: 12px;
+        background-color: #46a6e6ff;
+        text-align: center;
+        display: inline-block;
+        border-radius:5px;
+
+    }
+
+    .link {
+        Font-Size: 11px;
+        font-family: Helvetica, Arial, sans-serif;
+        display: inline-block;
+        margin-top: 30px;
+
+    }
+    </style>
+</body>
+
+</html>
